6571442e2c39197dac6c2d4cf663848046674b21
Sumanth181099 <65185434+Sumanth181099@users.noreply.github.com>
2021-04-14 23:38:24 +0530

    Create test_booking_system.c

---
+++ 3_Implementation/test/test_booking_system.c
@@ -0,0 +1,64 @@
+#include "unity.h"
+#include <train_details.h>
+
+/* Modify these two lines according to the project */
+#include <train_details.h>
+#define PROJECT_NAME    "ticket_booking_system"
+
+/* Prototypes for all the test functions */
+void test_update_train_schedule(void);
+void test_disp_train(void);
+void test_disp_cust(void);
+void test_reservation(void);
+void test_disp_train_details(void);
+void test_cust_details_entry(void);
+void test_total_cost(void);
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+/* Start of the application test */
+int main()
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_update_train_schedule);
+  RUN_TEST(test_disp_train);
+  RUN_TEST(test_disp_cust);
+  RUN_TEST(test_reservation);
+  RUN_TEST(test_disp_train_details);
+  RUN_TEST(test_cust_details_entry);
+  RUN_ENTRY(test_total_cost);
+
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
+
+/* Write all the test functions */ 
+void test_update_train_schedule(void) {
+ 
+}
+void test_disp_train(void) {
+ 
+}
+void test_disp_cust(void) {
+  
+}
+
+void test_reservation(void) {
+
+}
+
+void test_disp_train_details(void) {
+
+}
+void test_disp_cust_details_entry(void) {
+  
+}
+void test_total_cost(void) {
+  TEST_ASSERT_EQUAL(100, total_cost(2, 50));
+}
