47eff5d67268e5124a8fbbf0a4e35d35d8b5179b
Sumanth181099 <65185434+Sumanth181099@users.noreply.github.com>
2021-04-14 16:46:59 +0530

    Create train_structs.h

---
+++ 3_Implementation/inc/train_structs.h
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct train{
+	char name[30];
+	char from[30];
+	char to[30];
+	float cost;
+	int train_id;
+	int seats;
+};
+
+struct person{
+	char name[25];
+	char phone[15];
+	int id;
+	float cost;
+};
+
+struct booking{
+	int id;
+	int train_id;
+	int seats;
+};
